bff7129d8bcbd0741223b820517a9d26a20058e9
unknown <mhdshaiz.01@gmail.com>
2024-04-12 13:13:20 +0530

    first commit

---
+++ force-app/main/default/lwc/bikeCard/bikeCard.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+export default class BikeCard extends LightningElement {
+  name = 'Shaiz Electra X4';
+  description = 'A sweet bike built for comfort.';
+  category = 'Adventure';
+  material = 'Steel';
+  price = '40000 Rs';
+  pictureUrl = 'https://s3-us-west-1.amazonaws.com/sfdc-demo/ebikes/electrax4.jpg';
+}
